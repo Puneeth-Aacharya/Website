4c9d5172f9a214f034ed220ed1d9c85a3c5e9db0
Puneeth_Aacharya <68151137+Puneeth-Aacharya@users.noreply.github.com>
2022-02-09 21:56:14 +0530

    Update sendmail.js

--- sendmail.js
+++ sendmail.js
@@ -3,7 +3,7 @@ function sendMail()
 	var e=new Error('Something went Wrong !!!!');
 
 	Email.send({
-		SecureToken:"091bbbb1-8f21-4806-9a27-6d583ed9fd66",
+		SecureToken:"ba9c82d4-69f1-4ec5-898e-4dcd9b0ea976",
 		To:'websiteksp.738@gmail.com',
 		From:document.getElementById('cmail').value,
 		Subject:document.getElementById('cname').value,
